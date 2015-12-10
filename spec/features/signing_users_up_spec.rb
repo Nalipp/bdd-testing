974dd5b8d3df9eba86531d1555d4ac2163710a99
Nalipp <nateplipp@gmail.com>
2015-12-10 16:45:01 +0900

    update user _form styling

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "Users sign up" do
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    click_button "Create User"
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
